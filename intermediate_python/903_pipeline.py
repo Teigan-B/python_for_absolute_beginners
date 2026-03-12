4938ae5784e1da5b543fc33c0684a6c4905cd162
Teigan-B <teigan.berre.1998@gmail.com>
2026-03-12 14:16:30 +0000

    session 3 - add group and time diff calculations to 903 pipeline

--- intermediate_python/903_pipeline.py
+++ intermediate_python/903_pipeline.py
@@ -8,15 +8,17 @@ from sqlalchemy import (
     Table,
 )
 
-from utils import clean_903_table
+from utils import clean_903_table, group_calculation, time_difference
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 # Initialise session variables
-filepath = "/workspaces/python_for_absolute_beginners/intermediate_python/data/903_database.db"
+filepath = (
+    "/workspaces/python_for_absolute_beginners/intermediate_python/data/903_database.db"
+)
 
 collection_year = 2014
 collection_end = datetime(collection_year, 3, 31)
-
 
 
 # Read in 903 data from SQL database
@@ -27,7 +29,7 @@ inspection = inspect(engine_903)
 table_names = inspection.get_table_names()
 
 # Uncomment to check database connection
-#print(table_names)
+# print(table_names)
 
 metadata_903 = MetaData()
 
@@ -49,4 +51,27 @@ for table in table_names:
 for key, df in dfs.items():
     dfs[key] = clean_903_table(df, collection_end)
 
-print(dfs['header'])
+# Uncomment to check data cleaning
+# print(dfs['header'])
+
+# dict to store measure outputs
+measures = {}
+
+measures["Header by ethnicity"] = group_calculation(
+    dfs["header"], "ETHNICITY", "Header - Ethncities"
+)
+
+measures["Header by age"] = group_calculation(
+    dfs["header"], "AGE_BUCKETS", "Header - Age"
+)
+
+# print(pd.concat([measures["Header by ethnicity"], measures['Header by age']]))
+
+# dfs['missing']['MISSING_DURATION'] = dfs['missing'].apply(
+#     lambda x: relativedelta(x['MIS_END_dt'], x['MIS_START_dt']).normalized().days, axis=1
+# )
+
+dfs["missing"]["MISSING_DURATION"] = time_difference(
+    dfs["missing"]["MIS_START_dt"], dfs["missing"]["MIS_END_dt"]
+)
+print(dfs["missing"])
